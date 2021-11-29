374d43ad4ce1f732577b481b044919efb3498299
marv7000 <marv7000@web.de>
2021-11-29 20:51:52 +0100

    Update to 1.0

--- FastValveMaterial.py
+++ FastValveMaterial.py
@@ -1,20 +1,21 @@
-#import numpy
 from PIL import Image, ImageChops, ImageOps
 import time
 import os
 import sys
 import math
-from VTFLibEnums import ImageFlag
-import VTFLib
-import VTFLibEnums
+from VTFLibWrapper.VTFLibEnums import ImageFlag
+import VTFLibWrapper.VTFLib as VTFLib
+import VTFLibWrapper.VTFLibEnums as VTFLibEnums
 import numpy as np
 from ctypes import create_string_buffer
 from pathlib import Path
+import shutil
+from pathlib import Path
 
 vtf_lib = VTFLib.VTFLib()
-print("FastValveMaterial (v1027)\n")
-print("Usage: Add textures to a subfolder and group textures together for every material you want to create.")
 
+print("FastValveMaterial (v1129)\n")
+print("Usage: Add textures to a subfolder and group textures together for every material you want to create.")
 
 def get_file(path, name, ending):
     for file in os.listdir(path):
@@ -30,7 +31,7 @@ def find_unique_names():
     return listStuff
 
 
-def do_diffuse(cIm, aoIm): #
+def do_diffuse(cIm, aoIm):
     final_diffuse = ImageChops.multiply(cIm, aoIm)
     r,g,b,a = final_diffuse.split()
     a = a.convert('RGBA')
@@ -38,10 +39,11 @@ def do_diffuse(cIm, aoIm): #
     a = a.convert('L')
     color_spc = (r,g,b,a)
     final_diffuse = Image.merge("RGBA", color_spc)
-    export_texture(final_diffuse, (material_name+'_c.vtf'), 'DXT5')
-    final_diffuse.save((material_name + '_color.tga'), 'TGA' )
-    print("[MIWC] Diffuse exported")
-    #finalDiffuse.show()
+    export_texture(final_diffuse, (gunName+'_c.vtf'), 'DXT5')
+    Path(outPath).mkdir(parents=True, exist_ok=True)
+    shutil.move(gunName+'_c.vtf', os.path.join(os.getcwd(), outPath))
+    #final_diffuse.save((material_name + '_color.tga'), 'TGA' )
+    print("[FVM] Diffuse exported")
 
 def do_exponent(gIm):
     finalExponent = gIm
@@ -62,9 +64,10 @@ def do_exponent(gIm):
     colorSpc = (r,g,b,a)
     finalExponent = Image.merge('RGBA', colorSpc)
     export_texture(finalExponent, (gunName+'_m.vtf'), 'DXT1')
-    finalExponent.save( (gunName + '_m.tga'), 'TGA' )
-    print("[MIWC] Exponent exported")
-    #finalExponent.show()
+    Path(outPath).mkdir(parents=True, exist_ok=True)
+    shutil.move(gunName+'_m.vtf', outPath)
+    #finalExponent.save( (gunName + '_m.tga'), 'TGA' )
+    print("[FVM] Exponent exported")
 
 def do_normal(mt, nIm, gIm):
     finalNormal = nIm
@@ -73,9 +76,9 @@ def do_normal(mt, nIm, gIm):
     col = finalGloss.size[1]
     gamma = 1
     for x in range(1 , row):
-        print(math.ceil(x/row*100), "% exported", end="\r")
+        print("(", math.ceil(x/row*100),"%)", end="\r")
         for y in range(1, col):
-            value = doGamma(x,y,finalGloss, mt)
+            value = do_gamma(x,y,finalGloss, mt)
             finalGloss.putpixel((x,y), value)
 
     r,g,b,a = finalNormal.split()
@@ -84,9 +87,11 @@ def do_normal(mt, nIm, gIm):
     a = a.convert('L')
     colorSpc = (r,g,b,a)
     finalNormal = Image.merge('RGBA', colorSpc)
-    finalNormal.save((gunName+'_n.tga'), 'TGA')
+    #finalNormal.save((gunName+'_n.tga'), 'TGA')
     export_texture(finalNormal, (gunName+'_n.vtf'), 'RGBA8888')
-    print("[MIWC] Normal exported")
+    Path(outPath).mkdir(parents=True, exist_ok=True)
+    shutil.move(gunName+'_n.vtf', outPath)
+    print("[FVM] Normal exported")
 
 def do_gamma(x, y, im, mt):
     gamma = 1
@@ -127,10 +132,34 @@ def fix_scale_mismatch(rgbIm, target): #Resize the target image to be the same a
     else:
         return target
 
-def do_material(rgbIm, aoIm, nIm, gIm):
-    print("Creating material "+ material_name)
-
-
+def do_material(mName):
+    print("[FVM] Creating material '"+ mName + "'")
+    f = open(mName + ".vmt", 'w')
+    writer = ('"VertexLitGeneric"',
+    '\n{', 
+    '\n\t$basetexture ' + outPath + mName + "_c",
+    '\n\t$bumpmap ' + outPath + mName + "_n",
+    '\n\t$phongexponenttexture ' + outPath + mName + "_m",
+    '\n\t$color2	"[ .3 .3 .3 ]"',
+    '\n\t$blendtintbybasealpha "1"',
+    '\n\t$phong "1"',
+    '\n\t$phongboost "1"',
+    '\n\t$phongalbedotint "1"',
+    '\n\t"$PhongFresnelRanges" "[ 2 4 30 ]"',
+    '\n\t$envmap	"env_cubemap"',
+    '\n\t$basemapalphaenvmapmask "1"',
+    '\n\t$envmapfresnel "0.3"',
+    '\n\t$envmaptint "[ .035 .035 .035 ]"',
+    '\n\t"Proxies"', 
+    '\n\t{',
+    '\n\t\t"MwEnvMapTint"',
+    '\n\t\t{',
+    '\n\t\t\t"min" "0"',
+    '\n\t\t\t"max" "0.015"',
+    '\n\t\t}',
+    '\n\t}',
+    '\n}')
+    f.writelines(writer)
 
 def export_texture(texture, path, imageFormat=None):
     image_data = (np.asarray(texture)*-1) * 255
@@ -162,34 +191,34 @@ def export_texture(texture, path, imageFormat=None):
     vtf_lib.image_save(path)
     vtf_lib.image_destroy()
 
-path = input("[MIWC] Enter local search path (e.g. images): ")
-mt = int(input("[MIWC] Enter midtone value (e.g. 215): "))
-for name in findUniqueNames():
+path = input("[FVM] Enter local search path (e.g. images): ")
+outPath = input("[FVM] Enter subfolder path (e.g. export/materials/): ")
+mt = int(input("[FVM] Enter midtone value (e.g. 215): "))
+for name in find_unique_names():
     material_name = "export"
-    
+    print("[FVM] Loading:")
     try:
         gunName = name
         print(gunName)
-        colorSt = path + "/" + str(getFile(path, name, "rgb.tga"))
+        colorSt = path + "/" + str(get_file(path, name, "rgb.tga"))
         if colorSt == path + "/None":
-            colorSt = path + "/" + str(getFile(path, name, "c.tga"))
-        aoSt = path + "/" + str(getFile(path, name, "o.tga"))
-        metalSt = path + "/" + str(getFile(path, name, "alpha.tga"))
+            colorSt = path + "/" + str(get_file(path, name, "c.tga"))
+        aoSt = path + "/" + str(get_file(path, name, "o.tga"))
+        metalSt = path + "/" + str(get_file(path, name, "alpha.tga"))
         if metalSt == path + "/None":
-            metalSt = path + "/" + str(getFile(path, name, "s.tga"))
-        normalSt = path + "/" + str(getFile(path, name, "n.tga"))
-        glossSt = path + "/" + str(getFile(path, name, "g.tga"))
+            metalSt = path + "/" + str(get_file(path, name, "s.tga"))
+        normalSt = path + "/" + str(get_file(path, name, "n.tga"))
+        glossSt = path + "/" + str(get_file(path, name, "g.tga"))
     except FileNotFoundError:
-        input("[MIWC] [ERROR] Couldn't locate files with correct naming scheme (22)")
+        input("[FVM] [ERROR] Couldn't locate files with correct naming scheme (23)")
         sys.exit()
 
-    print("[MIWC] Loading:")
     print(colorSt)
     print(aoSt)
     print(metalSt)
     print(normalSt)
     print(glossSt)
-    print("-----------\n\n")
+    print("-----------\n")
     colorImage = Image.open(colorSt)
     aoImage = Image.open(aoSt)
     metalImage = Image.open(metalSt)
@@ -201,8 +230,8 @@ for name in findUniqueNames():
     normalImage = fix_scale_mismatch(colorImage, normalImage)
     glossImage = fix_scale_mismatch(colorImage, glossImage)
 
-    doDiffuse(colorImage, aoImage)
-    doExponent(glossImage)
+    do_diffuse(colorImage, aoImage)
+    do_exponent(glossImage)
     do_normal(mt, normalImage, glossImage)
-    do_material(colorImage, aoImage, metalImage, normalImage)
-    print("[MIWC] Conversion finished")
+    do_material(gunName)
+    print("[FVM] Conversion finished")
